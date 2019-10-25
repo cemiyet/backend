20415dfb46b70c8b549ab06a3d58916a456546ad
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-10-25 23:42:36 +0300

    added BaseEntity class. closes #4

---
+++ Core/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cemiyet.Core.Entities
+{
+    /// <summary>
+    /// Generic base class for the entities.
+    /// </summary>
+    /// <typeparam name="TId">Type for the identifier. Must be same with the Id of the User model.</typeparam>
+    public class BaseEntity<TId>
+    {
+        public TId Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ModificationDate { get; set; }
+        public TId CreatorId { get; set; }
+        public TId ModifierId { get; set; }
+    }
+}

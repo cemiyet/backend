5b20047e5b60dada82e17991c235238550bbc4e2
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-10-29 00:57:07 +0300

    added PagingExtensions for paging feature.

---
+++ Persistence/Extensions/PagingExtensions.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cemiyet.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cemiyet.Persistence.Extensions
+{
+    public static class PagingExtensions
+    {
+        public static async Task<List<T>> PagedToListAsync<T>(this IQueryable<T> query,
+            int page = 1, int pageSize = Constants.PageSize)
+        {
+            var skip = (page - 1) * pageSize;
+
+            return await query.Skip(skip).Take(pageSize).ToListAsync();
+        }
+
+        public static List<T> PagedToList<T>(this IQueryable<T> query, int page = 1,
+            int pageSize = Constants.PageSize)
+        {
+            var skip = (page - 1) * pageSize;
+
+            return query.Skip(skip).Take(pageSize).ToList();
+        }
+    }
+}

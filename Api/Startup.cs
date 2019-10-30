e0409c0321887af6134157ba35b9e29983cc3c85
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-10-30 20:24:00 +0300

    added openapi and redoc page for api documentation.

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -11,47 +11,62 @@ namespace Cemiyet.Api
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; }
+        private IConfigurationSection P { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            P = Configuration.GetSection("Project");
         }
-
-        public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<MainDataContext>(options =>
             {
-                options.UseNpgsql(Configuration.GetConnectionString("MainDataContext"));
+                options.UseNpgsql(
+                    Configuration.GetConnectionString("MainDataContext"));
             });
 
             services.AddControllers();
-            services.AddMediatR(typeof(Application.Genres.Queries.List.ListQuery).Assembly);
+
+            services.AddMediatR(
+                typeof(Application.Genres.Queries.List.ListQuery).Assembly);
+
+            services.AddOpenApiDocument(options =>
+            {
+                options.Title = P["Name"];
+                options.Version = P["Version"];
+                options.DocumentName = options.Version;
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            using (var serviceScope = app.ApplicationServices
+                .GetService<IServiceScopeFactory>().CreateScope())
             {
-                var context = serviceScope.ServiceProvider.GetRequiredService<MainDataContext>();
+                var context = serviceScope.ServiceProvider
+                    .GetRequiredService<MainDataContext>();
                 context.Database.Migrate();
                 MainDataContextSeed.Seed(context);
             }
 
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
+            else
+                app.UseHsts();
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 
-            app.UseEndpoints(endpoints =>
+            app.UseOpenApi(options => options.Path = "/docs/{documentName}/openapi.json");
+            app.UseReDoc(options =>
             {
-                endpoints.MapControllers();
+                options.Path = "/docs";
+                options.DocumentPath = "/docs/{documentName}/openapi.json";
             });
         }
     }

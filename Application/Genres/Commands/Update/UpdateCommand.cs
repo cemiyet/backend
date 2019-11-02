88ea98bfed7c58d0dcf78265d7d192fea831c6aa
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-11-02 12:48:54 +0300

    added Genres/UpdateCommandValidator. #14

--- Application/Genres/Commands/Update/UpdateCommand.cs
+++ Application/Genres/Commands/Update/UpdateCommand.cs
@@ -3,7 +3,6 @@ using MediatR;
 
 namespace Cemiyet.Application.Genres.Commands.Update
 {
-    // TODO (v0.1): create validator.
     public class UpdateCommand : IRequest
     {
         public Guid Id { get; set; }

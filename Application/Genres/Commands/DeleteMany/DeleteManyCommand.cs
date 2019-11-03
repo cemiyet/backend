267b11ead3d3984a1bb470a8a3ad133abbf21023
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-11-03 23:42:38 +0300

    added Genres/DeleteManyCommandValidator. #14

--- Application/Genres/Commands/DeleteMany/DeleteManyCommand.cs
+++ Application/Genres/Commands/DeleteMany/DeleteManyCommand.cs
@@ -3,7 +3,6 @@ using MediatR;
 
 namespace Cemiyet.Application.Genres.Commands.DeleteMany
 {
-    // TODO (v0.1): create validator.
     public class DeleteManyCommand : IRequest
     {
         public Guid[] Ids { get; set; }

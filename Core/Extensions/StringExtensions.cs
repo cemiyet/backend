2e86b6ae6fe7ba8a8cdc420f42d25f1766c4b894
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-11-04 19:37:26 +0300

    added StringExtensionsTests. #17

--- Core/Extensions/StringExtensions.cs
+++ Core/Extensions/StringExtensions.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Cemiyet.Core.Extensions
@@ -6,11 +7,9 @@ namespace Cemiyet.Core.Extensions
     {
         public static string ToSnakeCase(this string input)
         {
-            if (string.IsNullOrEmpty(input))
-                return input;
+            if (string.IsNullOrEmpty(input)) return input;
 
-            return Regex.Match(input, @"^_+") +
-                   Regex.Replace(input, @"([a-z0-9])([A-Z])", "$1_$2").ToLowerInvariant();
+            return Regex.Match(input, @"^_+") + Regex.Replace(input, @"([a-z0-9])([A-Z])", "$1_$2").ToLowerInvariant();
         }
     }
 }

6e493b2b5df9b8135f480bd050d8f7381b3361cb
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-11-04 16:08:14 +0300

    added Genres/AddCommandValidator. #14

--- Application/Genres/Commands/Add/AddCommand.cs
+++ Application/Genres/Commands/Add/AddCommand.cs
@@ -2,7 +2,6 @@ using MediatR;
 
 namespace Cemiyet.Application.Genres.Commands.Add
 {
-    // TODO (v0.1): create validator.
     public class AddCommand : IRequest
     {
         public string Name { get; set; }

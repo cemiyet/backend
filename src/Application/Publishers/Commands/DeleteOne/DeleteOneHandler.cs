8eb33436b847633f1fd56724dc6379f5d33a86a8
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-11-16 23:57:39 +0300

    added publishers/DeleteOne command classes. #22

---
+++ src/Application/Publishers/Commands/DeleteOne/DeleteOneHandler.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Cemiyet.Core.Exceptions;
+using Cemiyet.Persistence.Application.Contexts;
+using MediatR;
+
+namespace Cemiyet.Application.Publishers.Commands.DeleteOne
+{
+    public class DeleteOneHandler : IRequestHandler<DeleteOneCommand>
+    {
+        private readonly AppDataContext _context;
+
+        public DeleteOneHandler(AppDataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteOneCommand request, CancellationToken cancellationToken)
+        {
+            var publisher = await _context.Publishers.FindAsync(request.Id);
+
+            if (publisher == null)
+                throw new PublisherNotFoundException(request.Id);
+
+            _context.Remove(publisher);
+
+            var success = await _context.SaveChangesAsync() > 0;
+
+            if (success) return Unit.Value;
+
+            throw new Exception("Problem saving changes.");
+        }
+    }
+}

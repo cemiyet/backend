8d6211933aaee09caecc4b218da3a832ed4b66cc
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-11-21 22:30:00 +0300

    added seed for book and book edition entities. resolves #23

--- src/Persistence/Application/Contexts/AppDataContextSeed.cs
+++ src/Persistence/Application/Contexts/AppDataContextSeed.cs
@@ -13,11 +13,14 @@ namespace Cemiyet.Persistence.Application.Contexts
             SeedDimensions(context);
             SeedAuthors(context);
             SeedPublishers(context);
+            SeedBooks(context);
+            SeedBookEditions(context);
         }
 
         private static void SeedGenres(AppDataContext context)
         {
             if (context.Genres.Any()) return;
+
             var genres = new List<Genre>
             {
                 new Genre
@@ -110,6 +113,10 @@ namespace Cemiyet.Persistence.Application.Contexts
                 },
                 new Genre
                 {
+                    Name = "Tarih", CreationDate = DateTime.UtcNow
+                },
+                new Genre
+                {
                     Name = "Tarihi Roman", CreationDate = DateTime.UtcNow
                 }
             };
@@ -128,6 +135,10 @@ namespace Cemiyet.Persistence.Application.Contexts
                 new Dimension
                 {
                     Width = 10.5, Height = 15.9, CreationDate = DateTime.UtcNow
+                },
+                new Dimension
+                {
+                    Width = 10.5, Height = 17, CreationDate = DateTime.UtcNow
                 },
                 new Dimension
                 {
@@ -252,6 +263,93 @@ namespace Cemiyet.Persistence.Application.Contexts
             };
 
             context.Publishers.AddRange(publishers);
+            context.SaveChanges();
+        }
+
+        private static void SeedBooks(AppDataContext context)
+        {
+            if (context.Books.Any()) return;
+
+            var books = new List<Book>
+            {
+                new Book
+                {
+                    Title = "Nutuk",
+                    Description = @"Ey Türk Gençliği!
+
+                                    Birinci vazifen, Türk istiklâlini, Türk Cumhuriyetini, ilelebet, muhafaza ve müdafaa etmektir.
+                                    Mevcudiyetinin ve istikbalinin yegâne temeli budur. Bu temel, senin, en kıymetli hazinendir.
+                                    İstikbalde dahi, seni bu hazineden mahrum etmek isteyecek, dahilî ve haricî bedhahların olacaktır.
+                                    Bir gün, İstiklâl ve Cumhuriyeti müdafaa mecburiyetine düşersen, vazifeye atılmak için, içinde bulunacağın vaziyetin imkân ve şerâitini düşünmeyeceksin!
+                                    Bu imkân ve şerâit, çok nâmüsait bir mahiyette tezahür edebilir.
+                                    İstiklâl ve Cumhuriyetine kastedecek düşmanlar, bütün dünyada emsali görülmemiş bir galibiyetin mümessili olabilirler.
+                                    Cebren ve hile ile aziz vatanın, bütün kaleleri zaptedilmiş, bütün tersanelerine girilmiş, bütün orduları dağıtılmış ve memleketin her köşesi bilfiil işgal edilmiş olabilir.
+                                    Bütün bu şerâitten daha elîm ve daha vahim olmak üzere, memleketin dahilinde, iktidara sahip olanlar gaflet ve dalâlet ve hattâ hıyanet içinde bulunabilirler.
+                                    Hattâ bu iktidar sahipleri şahsî menfaatlerini, müstevlilerin siyasi emelleriyle tevhit edebilirler.
+                                    Millet, fakr ü zaruret içinde harap ve bîtap düşmüş olabilir.
+
+                                    Ey Türk istikbalinin evlâdı! İşte, bu ahval ve şerâit içinde dahi, vazifen;
+                                    Türk İstiklâl ve Cumhuriyetini kurtarmaktır!
+                                    Muhtaç olduğun kudret, damarlarındaki asil kanda mevcuttur!",
+                    Authors = new List<AuthorsBooks>
+                    {
+                        new AuthorsBooks
+                        {
+                            Author = context.Authors.Single(a => a.Name == "Mustafa Kemal" && a.Surname == "Atatürk")
+                        }
+                    },
+                    Genres = new List<BooksGenres>
+                    {
+                        new BooksGenres
+                        {
+                            Genre = context.Genres.Single(g => g.Name == "Anı")
+                        },
+                        new BooksGenres
+                        {
+                            Genre = context.Genres.Single(g => g.Name == "Anlatı")
+                        },
+                        new BooksGenres
+                        {
+                            Genre = context.Genres.Single(g => g.Name == "Tarih")
+                        }
+                    },
+                    CreationDate = DateTime.UtcNow
+                },
+            };
+
+            context.Books.AddRange(books);
+            context.SaveChanges();
+        }
+
+        private static void SeedBookEditions(AppDataContext context)
+        {
+            if (context.BookEditions.Any()) return;
+
+            var bookEditions = new List<BookEdition>
+            {
+                new BookEdition
+                {
+                    Isbn = "9789944888349",
+                    PageCount = 599,
+                    PrintDate = new DateTime(2019, 6, 28),
+                    CreationDate = DateTime.UtcNow,
+                    Publisher = context.Publishers.Single(p => p.Name == "Türkiye İş Bankası Kültür Yayınları"),
+                    Book = context.Books.Single(b => b.Title == "Nutuk"),
+                    Dimensions = context.Dimensions.Single(d => d.Width == 15.5 && d.Height == 23)
+                },
+                new BookEdition
+                {
+                    Isbn = "9789750820038",
+                    PageCount = 1197,
+                    PrintDate = new DateTime(2019, 11, 05),
+                    CreationDate = DateTime.UtcNow,
+                    Publisher = context.Publishers.Single(p => p.Name == "Yapı Kredi Yayınları"),
+                    Book = context.Books.Single(b => b.Title == "Nutuk"),
+                    Dimensions = context.Dimensions.Single(d => d.Width == 10.5 && d.Height == 17)
+                },
+            };
+
+            context.BookEditions.AddRange(bookEditions);
             context.SaveChanges();
         }
     }

f7c08cd2caf1b91d064f5c3cee8905a55104c74c
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-11-21 21:15:56 +0300

    added book-related exceptions. #23

---
+++ src/Core/Exceptions/BookEditionNotFoundException.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Cemiyet.Core.Exceptions
+{
+    public class BookEditionNotFoundException : Exception
+    {
+        public BookEditionNotFoundException(string bookEditionId) : base($"Could not found any edition of book with specified id: {bookEditionId}")
+        {
+        }
+
+        public BookEditionNotFoundException(IEnumerable<string> bookEditionIds) : base("Could not found any edition of book with specified ids.")
+        {
+        }
+
+        protected BookEditionNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public BookEditionNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ src/Core/Exceptions/BookNotFoundException.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Cemiyet.Core.Exceptions
+{
+    public class BookNotFoundException : Exception
+    {
+        public BookNotFoundException(Guid bookId) : base($"Could not found any book with specified id: {bookId}")
+        {
+        }
+
+        public BookNotFoundException(IEnumerable<Guid> bookIds) : base("Could not found any book with specified ids.")
+        {
+        }
+
+        protected BookNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public BookNotFoundException(string message) : base(message)
+        {
+        }
+
+        public BookNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}

a537771e43dcf46c631d64c58cf07c169824da1d
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-11-26 23:36:54 +0300

    added Books/ListEdition validator tests. #25

--- tests/Application.Tests/ValidatorTests/BooksValidatorTests.cs
+++ tests/Application.Tests/ValidatorTests/BooksValidatorTests.cs
@@ -1,6 +1,8 @@
 using System;
 using Cemiyet.Application.Books.Queries.List;
+using Cemiyet.Application.Books.Queries.ListEdition;
 using Cemiyet.Application.Books.Queries.Details;
+using Cemiyet.Application.Books.Queries.DetailsEdition;
 using FluentValidation.TestHelper;
 using Xunit;
 
@@ -9,12 +11,16 @@ namespace Cemiyet.Application.Tests.ValidatorTests
     public class BooksValidatorTests
     {
         private readonly ListQueryValidator _listQueryValidator;
+        private readonly ListEditionQueryValidator _listEditionQueryValidator;
         private readonly DetailsQueryValidator _detailsQueryValidator;
+        private readonly DetailsEditionQueryValidator _detailsEditionQueryValidator;
 
         public BooksValidatorTests()
         {
             _listQueryValidator = new ListQueryValidator();
+            _listEditionQueryValidator = new ListEditionQueryValidator();
             _detailsQueryValidator = new DetailsQueryValidator();
+            _detailsEditionQueryValidator = new DetailsEditionQueryValidator();
         }
 
         [Fact]
@@ -32,6 +38,24 @@ namespace Cemiyet.Application.Tests.ValidatorTests
         {
             _listQueryValidator.ShouldNotHaveValidationErrorFor(x => x.Page, 5);
             _listQueryValidator.ShouldNotHaveValidationErrorFor(x => x.PageSize, 50);
+        }
+
+        [Fact]
+        public void ListEditionQuery_ShouldHave_ValidationErrors()
+        {
+            _listEditionQueryValidator.ShouldHaveValidationErrorFor(x => x.Page, 0);
+            _listEditionQueryValidator.ShouldHaveValidationErrorFor(x => x.Page, -1);
+
+            _listEditionQueryValidator.ShouldHaveValidationErrorFor(x => x.PageSize, 0);
+            _listEditionQueryValidator.ShouldHaveValidationErrorFor(x => x.PageSize, -1);
+        }
+
+        [Fact]
+        public void ListEditionQuery_ShouldNotHave_ValidationErrors()
+        {
+            _listEditionQueryValidator.ShouldNotHaveValidationErrorFor(x => x.Id, Guid.NewGuid());
+            _listEditionQueryValidator.ShouldNotHaveValidationErrorFor(x => x.Page, 5);
+            _listEditionQueryValidator.ShouldNotHaveValidationErrorFor(x => x.PageSize, 50);
         }
 
         [Fact]

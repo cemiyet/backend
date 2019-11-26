9173a0d6c31a5dc1c5c3659248329efd0c3311f1
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-11-26 23:22:27 +0300

    added books list and details validator tests. #25

---
+++ tests/Application.Tests/ValidatorTests/BooksValidatorTests.cs
@@ -0,0 +1,50 @@
+using System;
+using Cemiyet.Application.Books.Queries.List;
+using Cemiyet.Application.Books.Queries.Details;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace Cemiyet.Application.Tests.ValidatorTests
+{
+    public class BooksValidatorTests
+    {
+        private readonly ListQueryValidator _listQueryValidator;
+        private readonly DetailsQueryValidator _detailsQueryValidator;
+
+        public BooksValidatorTests()
+        {
+            _listQueryValidator = new ListQueryValidator();
+            _detailsQueryValidator = new DetailsQueryValidator();
+        }
+
+        [Fact]
+        public void ListQuery_ShouldHave_ValidationErrors()
+        {
+            _listQueryValidator.ShouldHaveValidationErrorFor(x => x.Page, 0);
+            _listQueryValidator.ShouldHaveValidationErrorFor(x => x.Page, -1);
+
+            _listQueryValidator.ShouldHaveValidationErrorFor(x => x.PageSize, 0);
+            _listQueryValidator.ShouldHaveValidationErrorFor(x => x.PageSize, -1);
+        }
+
+        [Fact]
+        public void ListQuery_ShouldNotHave_ValidationErrors()
+        {
+            _listQueryValidator.ShouldNotHaveValidationErrorFor(x => x.Page, 5);
+            _listQueryValidator.ShouldNotHaveValidationErrorFor(x => x.PageSize, 50);
+        }
+
+        [Fact]
+        public void DetailsQuery_ShouldHave_ValidationErrors()
+        {
+            _detailsQueryValidator.ShouldHaveValidationErrorFor(x => x.Id, default(Guid));
+        }
+
+        [Fact]
+        public void DetailsQuery_ShouldNotHave_ValidationErrors()
+        {
+            _detailsQueryValidator.ShouldNotHaveValidationErrorFor(x => x.Id, Guid.NewGuid());
+        }
+    }
+}
+

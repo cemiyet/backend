40ab58a87893680300d7cfe3e901ec9f3399e5be
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-12-01 22:48:35 +0300

    refactored DimensionsController integration tests.

--- tests/Api.Tests/DimensionsControllerTests.cs
+++ tests/Api.Tests/DimensionsControllerTests.cs
@@ -1,17 +1,15 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using Cemiyet.Api.Tests.Extensions;
 using Cemiyet.Application.Dimensions.Commands.DeleteMany;
 using Cemiyet.Core.Entities;
 using Cemiyet.Persistence.Application.Contexts;
 using Cemiyet.Persistence.Application.ViewModels;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json;
 using Xunit;
 
 namespace Cemiyet.Api.Tests
@@ -34,7 +32,6 @@ namespace Cemiyet.Api.Tests
         public async Task Add_WithoutCorrectData_ShouldReturn_BadRequest()
         {
             var response = await _httpClient.PostAsJsonAsync("dimensions/", default(Dimension));
-
             Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
         }
 
@@ -46,43 +43,32 @@ namespace Cemiyet.Api.Tests
                 Width = 1.5,
                 Height = 2.5
             });
-
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
 
         [Fact]
         public async Task List_WithoutCorrectPaging_ShouldReturn_BadRequest()
         {
-            var response = await _httpClient.GetAsync("dimensions?page=-1&pageSize=-5");
-            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            await _httpClient.AssertedGetAsync("dimensions?page=-1&pageSize=-5", HttpStatusCode.BadRequest);
         }
 
         [Fact]
         public async Task List_WithoutPaging_ShouldReturn_DefaultPagedResult()
         {
-            var response = await _httpClient.GetAsync("dimensions");
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-
-            var responseData = await response.Content.ReadAsAsync<List<DimensionViewModel>>();
-            Assert.NotEmpty(responseData);
+            await _httpClient.AssertedGetEntityListFromUri<DimensionViewModel>("dimensions");
         }
 
         [Fact]
         public async Task Details_WithoutCorrectId_ShouldReturn_BadRequest()
         {
-            var response = await _httpClient.GetAsync($"dimensions/{Guid.NewGuid()}");
-            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            await _httpClient.AssertedGetAsync($"dimensions/{Guid.NewGuid()}", HttpStatusCode.BadRequest);
         }
 
         [Fact]
         public async Task Details_WithCorrectId_ShouldReturn_DimensionObject()
         {
-            var dimensionsResponse = await _httpClient.GetAsync("dimensions");
-            var dimensions = await dimensionsResponse.Content.ReadAsAsync<List<DimensionViewModel>>();
-
-            var response = await _httpClient.GetAsync($"dimensions/{dimensions.First().Id}");
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-
+            var dimensions = await _httpClient.AssertedGetEntityListFromUri<DimensionViewModel>("dimensions");
+            var response = await _httpClient.AssertedGetAsync($"dimensions/{dimensions.First().Id}", HttpStatusCode.OK);
             var responseData = await response.Content.ReadAsAsync<DimensionViewModel>();
             Assert.NotNull(responseData);
         }
@@ -97,9 +83,7 @@ namespace Cemiyet.Api.Tests
         [Fact]
         public async Task Update_WithoutCorrectData_ShouldReturn_BadRequest()
         {
-            var dimensionsResponse = await _httpClient.GetAsync("dimensions");
-            var dimensions = await dimensionsResponse.Content.ReadAsAsync<List<DimensionViewModel>>();
-
+            var dimensions = await _httpClient.AssertedGetEntityListFromUri<DimensionViewModel>("dimensions");
             var response = await _httpClient.PutAsJsonAsync($"dimensions/{dimensions.First().Id}", default(Dimension));
             Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
 
@@ -113,51 +97,29 @@ namespace Cemiyet.Api.Tests
         [Fact]
         public async Task Update_WithCorrectData_ShouldReturn_OK()
         {
-            var dimensionsResponse = await _httpClient.GetAsync("dimensions");
-            var dimensions = await dimensionsResponse.Content.ReadAsAsync<List<DimensionViewModel>>();
-
+            var dimensions = await _httpClient.AssertedGetEntityListFromUri<DimensionViewModel>("dimensions");
             var response = await _httpClient.PutAsJsonAsync($"dimensions/{dimensions.Last().Id}", new
             {
                 Width = 1.2,
                 Height = 1.6
             });
-
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
 
         [Fact]
         public async Task UpdatePartially_WithoutCorrectData_ShouldReturn_BadRequest()
         {
-            var dimensionsResponse = await _httpClient.GetAsync("dimensions");
-            var dimensions = await dimensionsResponse.Content.ReadAsAsync<List<DimensionViewModel>>();
-
-            var request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Patch,
-                RequestUri = new Uri(_httpClient.BaseAddress + $"dimensions/{dimensions.First().Id}"),
-                Content = new StringContent(JsonConvert.SerializeObject(new { }), Encoding.UTF8, "application/json")
-            };
-
-            var response = await _httpClient.SendAsync(request);
-            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            var dimensions = await _httpClient.AssertedGetEntityListFromUri<DimensionViewModel>("dimensions");
+            await _httpClient.AssertedSendRequestMessageAsync(HttpMethod.Patch, $"dimensions/{dimensions.First().Id}",
+                                                              new { }, HttpStatusCode.BadRequest);
         }
 
         [Fact]
         public async Task UpdatePartially_WithCorrectData_ShouldReturn_OK()
         {
-            var dimensionsResponse = await _httpClient.GetAsync("dimensions");
-            var dimensions = await dimensionsResponse.Content.ReadAsAsync<List<DimensionViewModel>>();
-
-            var request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Patch,
-                RequestUri = new Uri(_httpClient.BaseAddress + $"dimensions/{dimensions.First().Id}"),
-                Content = new StringContent(JsonConvert.SerializeObject(new { Width = 5.0 }), Encoding.UTF8,
-                                            "application/json")
-            };
-
-            var response = await _httpClient.SendAsync(request);
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var dimensions = await _httpClient.AssertedGetEntityListFromUri<DimensionViewModel>("dimensions");
+            await _httpClient.AssertedSendRequestMessageAsync(HttpMethod.Patch, $"dimensions/{dimensions.First().Id}",
+                                                              new { Width = 5.0 }, HttpStatusCode.OK);
         }
 
         [Fact]
@@ -170,9 +132,7 @@ namespace Cemiyet.Api.Tests
         [Fact]
         public async Task DeleteOne_WithCorrectId_ShouldReturn_OK()
         {
-            var dimensionsResponse = await _httpClient.GetAsync("dimensions");
-            var dimensions = await dimensionsResponse.Content.ReadAsAsync<List<DimensionViewModel>>();
-
+            var dimensions = await _httpClient.AssertedGetEntityListFromUri<DimensionViewModel>("dimensions");
             var response = await _httpClient.DeleteAsync($"dimensions/{dimensions.Last().Id}");
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
@@ -180,36 +140,20 @@ namespace Cemiyet.Api.Tests
         [Fact]
         public async Task DeleteMany_WithoutCorrectIds_ShouldReturn_BadRequest()
         {
-            string[] ids = { Guid.NewGuid().ToString(), Guid.NewGuid().ToString() };
-
-            var request = new HttpRequestMessage
+            await _httpClient.AssertedSendRequestMessageAsync(HttpMethod.Delete, "dimensions", new []
             {
-                Method = HttpMethod.Delete,
-                RequestUri = new Uri(_httpClient.BaseAddress + "dimensions"),
-                Content = new StringContent(JsonConvert.SerializeObject(ids), Encoding.UTF8, "application/json")
-            };
-
-            var response = await _httpClient.SendAsync(request);
-            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+                Guid.NewGuid().ToString(), Guid.NewGuid().ToString()
+            }, HttpStatusCode.BadRequest);
         }
 
         [Fact]
         public async Task DeleteMany_WithCorrectIds_ShouldReturn_OK()
         {
-            var dimensionsResponse = await _httpClient.GetAsync("dimensions");
-            var dimensions = await dimensionsResponse.Content.ReadAsAsync<List<DimensionViewModel>>();
-
-            var dmc = new DeleteManyCommand { Ids = dimensions.TakeLast(2).Select(g => g.Id).ToArray() };
-
-            var request = new HttpRequestMessage
+            var dimensions = await _httpClient.AssertedGetEntityListFromUri<DimensionViewModel>("dimensions");
+            await _httpClient.AssertedSendRequestMessageAsync(HttpMethod.Delete, "dimensions", new DeleteManyCommand
             {
-                Method = HttpMethod.Delete,
-                RequestUri = new Uri(_httpClient.BaseAddress + "dimensions"),
-                Content = new StringContent(JsonConvert.SerializeObject(dmc), Encoding.UTF8, "application/json")
-            };
-
-            var response = await _httpClient.SendAsync(request);
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Ids = dimensions.TakeLast(2).Select(g => g.Id).ToArray()
+            }, HttpStatusCode.OK);
         }
     }
 }

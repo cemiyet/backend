7483a3b88e5a7cff825986a2ba559c09fb696d7a
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-12-01 22:18:30 +0300

    added AssertedGetAsync method to HttpClientExtensions.

--- tests/Api.Tests/Extensions/HttpClientExtensions.cs
+++ tests/Api.Tests/Extensions/HttpClientExtensions.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Xunit;
 
 namespace Cemiyet.Api.Tests.Extensions
 {
@@ -17,6 +19,13 @@ namespace Cemiyet.Api.Tests.Extensions
                 RequestUri = new Uri(client.BaseAddress + uri),
                 Content = new StringContent(JsonConvert.SerializeObject(content), Encoding.UTF8, "application/json")
             });
+        }
+
+        public static async Task<HttpResponseMessage> AssertedGetAsync(this HttpClient client, string uri, HttpStatusCode code)
+        {
+            var response = await client.GetAsync(uri);
+            Assert.Equal(code, response.StatusCode);
+            return response;
         }
     }
 }

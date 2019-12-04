bbc23b5fa400dc2f1eaf19a353b1c2c964ea29ce
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-12-04 23:17:19 +0300

    added validator tests for series/list query. #29

---
+++ tests/Application.Tests/ValidatorTests/SeriesValidatorTests.cs
@@ -0,0 +1,36 @@
+using System;
+using Cemiyet.Application.Series.Queries.List;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace Cemiyet.Application.Tests.ValidatorTests
+{
+    public class SeriesValidatorTests
+    {
+        private readonly ListQueryValidator _listQueryValidator;
+
+        public SeriesValidatorTests()
+        {
+            _listQueryValidator = new ListQueryValidator();
+        }
+
+        [Theory]
+        [InlineData(0, -1)]
+        [InlineData(-1, 0)]
+        public void ListQuery_ShouldHave_ValidationErrors(int pageValue, int pageSizeValue)
+        {
+            _listQueryValidator.ShouldHaveValidationErrorFor(x => x.Page, pageValue);
+            _listQueryValidator.ShouldHaveValidationErrorFor(x => x.PageSize, pageSizeValue);
+        }
+
+        [Theory]
+        [InlineData(5, 50)]
+        [InlineData(50, 5)]
+        public void ListQuery_ShouldNotHave_ValidationErrors(int pageValue, int pageSizeValue)
+        {
+            _listQueryValidator.ShouldNotHaveValidationErrorFor(x => x.Page, pageValue);
+            _listQueryValidator.ShouldNotHaveValidationErrorFor(x => x.PageSize, pageSizeValue);
+        }
+    }
+}
+
